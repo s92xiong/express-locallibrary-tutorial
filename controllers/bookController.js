f60b75f8470b88571f8f6fa723f2255b57fc7768
Sheuh Xiong <sheuhxiong@gmail.com>
2021-06-30 16:26:51 -0400

    Updated bookController  by adding async parallel methods, updated index.pug to render instances counts

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,32 @@
+// Import  all the models to get our counts of documents
 const Book = require('../models/book');
+const Author = require("../models/author");
+const Genre = require("../models/genre");
+const BookInstance = require("../models/bookinstance");
 
-exports.index = (req, res) => {
-  res.send('NOT IMPLEMENTED: Site Home Page');
+// Import the async module
+const async = require("async");
+
+exports.index = (req, res, next) => {
+  async.parallel({
+    book_count: function(callback) {
+      Book.countDocuments({}, callback);
+    },
+    book_instance_count: function(callback) {
+      BookInstance.countDocuments({}, callback);
+    },
+    book_instance_available_count: function(callback) {
+      BookInstance.countDocuments({ status: "Available" }, callback);
+    },
+    author_count: function(callback) {
+      Author.countDocuments({}, callback);
+    },
+    genre_count: function(callback) {
+      Genre.countDocuments({}, callback);
+    }
+  }, function(err, results) {
+    res.render("index", { title: "Local Library Home", error: err, data: results });
+  });
 };
 
 // Display list of all books.
