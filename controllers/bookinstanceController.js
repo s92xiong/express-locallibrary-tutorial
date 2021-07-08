1e8bb580b6025fbdba87db4ea57c0668007c5193
Sheuh Xiong <sheuhxiong@gmail.com>
2021-07-08 11:35:52 -0400

    Added logic to render pug template for bookinstance form, as wella s added logic to create post for bookinstance in controller

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,4 +1,6 @@
 const BookInstance = require("../models/bookinstance");
+const Book = require("../models/book");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all BookInstances
 exports.bookinstance_list = (req, res, next) => {
@@ -24,15 +26,51 @@ exports.bookinstance_detail = (req, res, next) => {
   });
 };
 
-// Display BookInstance create form on GET.
-exports.bookinstance_create_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: BookInstance create GET');
+// Display BookInstance create form on GET. The controller gets a list of all books (book_list) and passes it to the view
+exports.bookinstance_create_get = (req, res, next) => {
+  Book.find({}, "title").exec((err, books) => {
+    if (err) return next(err);
+    res.render("bookinstance_form", { title: "Create BookInstance", book_list: books });
+  });
 };
 
 // Handle BookInstance create on POST.
-exports.bookinstance_create_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: BookInstance create POST');
-};
+exports.bookinstance_create_post = [
+  // Validate and sanitise fields
+    // We can use optional() to run a subsequent validation only if a field has been entered (this allows us to validate optional fields)
+    // Check that the optional date for due_back is an ISO8601-compliant date (the checkFalsy flag means that we'll accept either an empty string or null as an empty value)
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified").trim().isLength({ min: 1 }).escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date").optional({ checkFalsy: true }).isISO8601.toDate(),
+
+  // Process request after validation and sanitization
+  (req, res, next) => {
+    // Extract the validation errors from a request
+    const errors = validationResult(req);
+
+    // Create a BookInstance object w/ escaped and trimmed data
+    const bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back
+    });
+
+    if (!errors.isEmpty()) {
+      Book.find({}, "title").exec((err, books) => {
+        if (err) return next(err);
+        return res.render("bookinstance_form", { title: "Create BookInstance", book_list: books, selected_book: bookinstance.book._id, errors: errors.array(), bookinstance: bookinstance });
+      });
+    }
+
+    // Form Data is valid
+    bookinstance.save((err) => {
+      if (err) return next(err);
+      res.redirect(bookinstance.url);
+    });
+  },
+];
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = (req, res) => {
